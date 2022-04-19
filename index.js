d7e415566c7245643f02efee8ce4c90e9275f5f6
NDICHU <ndichumuriithi@gmail.com>
2022-04-19 14:21:16 +0300

    commit tree b29073670152d99b840130e721bd85fa7fca9035

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded",function(){
+    DOMMANUPILATION();
+});
+function DOMMANUPILATION(){
+    document.getElementById("text").innerHTML="This is really cool!";
+}
